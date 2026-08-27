9b3b4a3c128889f839001992b959fd8ce530d88b
王绍龙 <1853409216@qq.com>
2026-08-27 14:17:23 +0800

    Revert "fix: trigger mode prompt on input switch"
    This reverts commit 2de1d5b0fc6e1e22eb2e2efc576e31e92cdb12f9.

--- adapter/fcitx5/src/inputmethod.cpp
+++ adapter/fcitx5/src/inputmethod.cpp
@@ -229,10 +229,6 @@ void ModernIMEInputMethod::activate(const fcitx::InputMethodEntry &,
     if (auto *contextState = state(event.inputContext()); contextState != nullptr) {
         contextState->controller().setActive(true);
     }
-    if (event.inputContext() != nullptr) {
-        event.inputContext()->updateUserInterface(
-            fcitx::UserInterfaceComponent::StatusArea, true);
-    }
 }
 
 void ModernIMEInputMethod::deactivate(const fcitx::InputMethodEntry &entry,
@@ -240,10 +236,6 @@ void ModernIMEInputMethod::deactivate(const fcitx::InputMethodEntry &entry,
     reset(entry, event);
     if (auto *contextState = state(event.inputContext()); contextState != nullptr) {
         contextState->controller().setActive(false);
-    }
-    if (event.inputContext() != nullptr) {
-        event.inputContext()->updateUserInterface(
-            fcitx::UserInterfaceComponent::StatusArea, true);
     }
 }
 
